0eecdf325feea6e5f2f9c82b3855c18bf03d7b67
amit_chaman <amit_chaman@AmitChaman.cms.co.in>
2025-02-03 12:39:11 +0530

    changes added

--- src/constants/responseMessages.js
+++ src/constants/responseMessages.js
@@ -6,21 +6,6 @@ export const RESPONSE_MESSAGES = {
   //     UNAUTHORIZED: "You are not authorized to access this resource.",
   //   }
 
-<<<<<<< Updated upstream
-    ERROR:{
-        USER_ID_MISSING: "UserId is required.",
-        MISSING_FILTER:"filter is required.",
-        MISSING_TAB_VALUE:"Tabvalue is not provided",
-        INVALIDFILTER:"Invalid filter provided.",
-        NOATTENDANCERECORDS:'No attendance records found for the given range.',
-        SERVERERROR:"An error occurred while fetching attendance analytics.",
-        INVALID_REQUEST: "Invalid request date and ucc number is required",
-        INVALID_TYPE: "Please provide a proper type value"
-    },
-    SUCCESS:{
-         ANALYTICSFETCHED: "Attendance analytics fetched successfully."
-    }
-=======
   ERROR: {
     USER_ID_MISSING: 'UserId is required.',
     MISSING_FILTER: 'filter is required.',
@@ -33,5 +18,4 @@ export const RESPONSE_MESSAGES = {
   SUCCESS: {
     ANALYTICSFETCHED: 'Attendance analytics fetched successfully.',
   },
->>>>>>> Stashed changes
 }
