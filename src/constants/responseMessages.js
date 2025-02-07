92e0f6bbdff4a2e097cab0fc8f67b35843d567c3
amit_chaman <amit_chaman@AmitChaman.cms.co.in>
2025-02-07 15:38:04 +0530

    added pagination

--- src/constants/responseMessages.js
+++ src/constants/responseMessages.js
@@ -18,7 +18,9 @@ export const RESPONSE_MESSAGES = {
     INVALID_REQUEST: "Invalid request date and ucc number is required",
     INVALID_TYPE: "Please provide a proper type value",
     ERROR_DB_FETCH: "Error Occured while fetching data from DB",
-    CENTERLINES_ERROR: "Error Occured while fetching data from DB for centerlines."
+    CENTERLINES_ERROR: "Error Occured while fetching data from DB for centerlines.",
+    MISSING_TAB_VALUE : "TabValue must be me and myteam",
+    LAST_14_DAYS : "Date must be 14"
   },
   SUCCESS: {
       ANALYTICSFETCHED: 'Attendance analytics fetched successfully.',
