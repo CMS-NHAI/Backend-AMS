160b4f0c7f8adb4254a598ac034e702cd189382a
Deepanshu Rajput <deepanshu_r@cms.co.in>
2025-02-12 17:56:20 +0530

    added marked in count api

--- src/validations/attendaceValidation.js
+++ src/validations/attendaceValidation.js
@@ -1,26 +1,25 @@
 import Joi from 'joi'
 import moment from 'moment';
 
-const today = moment().format('YYYY-MM-DD');
-const fourteenDaysAgo = moment().subtract(14, 'days').format('YYYY-MM-DD');
+// const today = moment().format('YYYY-MM-DD');
+// const fourteenDaysAgo = moment().subtract(14, 'days').format('YYYY-MM-DD');
 
 export const markInAttendaceCountSchema =Joi.object({
   tabValue: Joi.string()
-    .valid('myteam')
+  .valid('myteam')
+  .required()
+  .messages({
+    'any.only': "tabValue must be 'myteam'",
+    'string.base': 'tabValue must be a string',
+    'any.required': 'tabValue is required',
+  }),
+  dayFilter: Joi.string()
+    .valid("today", "yesterday")
     .required()
-    .messages({
-      'any.only': "tabValue must be 'myteam'",
-      'string.base': 'tabValue must be a string',
-      'any.required': 'tabValue is required',
-    }),
-  date: Joi.string()
-    .valid(today, fourteenDaysAgo)
-    .required()
-    .messages({
-      'any.only': `Date must be '${today}' (today) or '${fourteenDaysAgo}' (14 days ago)`,
-      'string.base': 'Date must be a string in YYYY-MM-DD format',
-      'any.required': 'Date is required',
-    }),
+    .messages({ 
+      "any.only": "dayFilter must be 'today' or 'yesterday'" ,
+      'any.required': 'tabValue is required'
+    })
 })
 
 export const markAttendaceSchema = Joi.object({
