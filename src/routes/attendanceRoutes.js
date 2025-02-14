20c78cb92a9b9432d20886c804ec2c5a0779c744
deepak j <Deepak_J@DeepakJ.cms.co.in>
2025-02-14 16:06:32 +0530

    Added imports

--- src/routes/attendanceRoutes.js
+++ src/routes/attendanceRoutes.js
@@ -1,7 +1,7 @@
 import express from 'express'
 // import { getUserDetails } from "../services/userService";
 
-import { getAttendanceOverview , getAttendanceDetails, getAllProjects,getTeamAttendanceCount,markAttendance,markOutAttendance, checkedInEmployees, fetchEmployeesByProject} from '../controllers/attendanceController.js'
+import { getAttendanceOverview , getAttendanceDetails, getAllProjects,getTeamAttendanceCount,markAttendance,markOutAttendance, checkedInEmployees,getUserTodayAttendanceData, fetchEmployeesByProject} from '../controllers/attendanceController.js'
 import {validate} from '../middlewares/validate.js'
 import { validateToken } from '../middlewares/validateToken.js'
 import { fetchlocationBydate, fetchNearestProject } from '../controllers/locationController.js'
